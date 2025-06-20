48b4f5ec6a58da86bbc37be7c4c401cecad90688
Steven <stevenrivera127@gmail.com>
2025-06-19 19:57:52 -0500

    casi finilizacion de fundamentos

--- operador-ternario.js
+++ operador-ternario.js
@@ -1,50 +1,40 @@
-const fechaActual = new Date();
-let diaSemana = ahora.getDay(); 
-let horaActual = ahora.getHours(); 
+ const fechaActual=new Date();
+let diaSemana=fechaActual.getDay();
+let horaActual=fechaActual.getHours();
 
-let horadeApertura;
+let horaApertura;
+let horaCierre=18;
 let mensaje;
 
-/* if(diaSemana===0 || diaSemana===6){
-console.log("fin de semana");
-horadeApertura=9;
-}else{
-    console.log("dia de la semana");
-    horadeApertura= 11;
-}
+diaSemana=0;
+horaActual=9;
 
-if(horaActual >= horadeApertura){
-    mensaje="bienvenido, esta abierto";
-}else{
-    mensaje=`lo sentimos,nuestra tienda esta cerrrada. 
-    ${horadeApertura} horas`;
-} */
+console.log("fecha y hora actual: ", fechaActual);
+console.log("dia actual: ", diaSemana);
+console.log("hora actual: ", horaActual);
+// if(diaSemana==0||diaSemana==6){
+//     console.log("Fin de semana");
+//    horaApertura=9;
+// }
+// else{
+//     console.log("Dia de semana");
+//     horaApertura=11;
+// }
 
-horaApertura([0,6].includes(diaSemana)) ? 9 : 11;
-if (horaActual >= horadeApertura){ 
-mensaje = "!bienvenido¡ Nuestra tienda esta abierta";
-}else{
-   mensaje = `lo sentimos, nuestra tienda esta cerrada en estos momentos. Abrimos a las ${horadeApertura} horas`;
-}
+horaApertura=([0,6].includes(diaSemana))?9:11;
+// if([0,6].includes(diaSemana)){
+//     console.log("Fin de semana");
+//    horaApertura=9;
+// }
+// else{
+//     console.log("Dia de semana");
+//     horaApertura=11;
+// }
 
-
-
-
-
-
-/* if(horaActual >= horadeApertura){
-    mensaje="bienvenido, esta abierto";
-}else{
-    mensaje=`lo sentimos,nuestra tienda esta cerrrada. 
-    ${horadeApertura} horas`;
-}
- */
-
-
-
-mensaje = (horaActual >= horadeApertura)
-             ?"bienvenido, esta abierto"
-             :mensaje=`lo sentimos,nuestra tienda esta cerrrada. 
-             ${horadeApertura} horas`;
-
-             console.log(mensaje);
+mensaje=(horaActual>=horaApertura)?"¡Bienvenido! Nuestra tienda está abierta.":"Lo sentimos, nuestra tienda está cerrada en este momento. Abrimos a las "+horaApertura+ "horas";
+// if(horaActual>=horaApertura){
+//     mensaje="¡Bienvenido! Nuestra tienda está abierta.";
+// }else{
+//     mensaje="Lo sentimos, nuestra tienda está cerrada en este momento. Abrimos a las "+horaApertura+ "horas";
+// }
+console.log(horaApertura, mensaje);
---
+++ practica-alternativa-if-else.js
@@ -0,0 +1,22 @@
+const diasSemana ={
+    0: "lunes",
+   1:  "martes",
+   3: "miercoles",
+   4: "jueves",
+   5:  "viernes",
+   6: "sabado",
+   7: "domingo",
+   
+   };
+   
+   console.log(diasSemana[1]|| "dia no valido"); 
+   console.log(diasSemana[2]|| "dia no valido");
+   console.log(diasSemana[4]|| "dia no valido");
+   console.log(diasSemana[9]|| "dia no valido");
+
+ const diasSeman =["domingo","lunes","martes","miercoles","jueves",
+"viernes","sabado","domingo"];
+console.log(diasSemana[1]|| "dia no valido"); 
+console.log(diasSemana[2]|| "dia no valido");
+console.log(diasSemana[4]|| "dia no valido");
+console.log(diasSemana[9]|| "dia no valido");
